6c9d79068ba6626c51b2fdad939ea91bf913b5e1
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2026-02-20 04:12:46 -0500

    added Java code for accesing online library at users request

---
+++ ProgramFiles/Acess.java
@@ -0,0 +1,8 @@
+package ProgramFiles;
+
+public class Acess {
+    public static void main(String[] args) {
+        //try catch code for accessing a certain word from a library (MerriamWebster Dictionary, etc)
+        //user searches a word, and it wll return results from multiple dictionarys, defintions, interpretations and everything else
+    }
+}
